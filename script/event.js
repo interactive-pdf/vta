54326d3a5a93a6588e47a006ff558a66feab636c
azputra <ahmadzp102@gmail.com>
2022-08-22 02:24:35 +0700

    update fe new page

--- script/event.js
+++ script/event.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // $("#magazine").css({"margin-top":screen.height/2});
 
     $("#magazine").bind('start', function (event, pageObject, corner){
-        if (pageObject.page === 8 || pageObject.page === 9 || pageObject.page === 10 || pageObject.page === 11) {
+        if (pageObject.page === 24 || pageObject.page === 25 || pageObject.page === 26 || pageObject.page === 27) {
             if (corner == 'tl' || corner == 'tr' || corner == 'bl' || corner == 'br'){
                 event.preventDefault();
             }
@@ -23,7 +23,11 @@ $(document).ready(function() {
                     if (page === 1) {
                         $("#magazine").css({"z-index":20});
                         $("#navigate-cover").css({"z-index": 30})
+                        $("#index-clear").css({"z-index": 0})
+                    } else if (page === 4) {
+                        $("#index-clear").css({"z-index": 30})
                     } else {
+                        $("#index-clear").css({"z-index": 0})
                         $("#magazine").css({"z-index":0});
                         $("#navigate-cover").css({"z-index": 0})
                     }
@@ -61,33 +65,83 @@ $(document).ready(function() {
         removeNextPrev()
     });
 
-    $("#page8").click(function() {
-        $('#magazine').turn("page", 8)
+    $("#page6").click(function() {
+        $('#magazine').turn("page", 6)
+        removeNextPrev()
+    });
+
+    $("#page9").click(function() {
+        $('#magazine').turn("page", 9)
+        removeNextPrev()
+    });
+
+    $("#page11").click(function() {
+        $('#magazine').turn("page", 11)
+        removeNextPrev()
+    });
+
+    $("#page14").click(function() {
+        $('#magazine').turn("page", 14)
+        removeNextPrev()
+    });
+
+    $("#page16").click(function() {
+        $('#magazine').turn("page", 16)
+        removeNextPrev()
+    });
+
+    $("#page17").click(function() {
+        $('#magazine').turn("page", 17)
+        removeNextPrev()
+    });
+
+    $("#page19").click(function() {
+        $('#magazine').turn("page", 19)
+        removeNextPrev()
+    });
+
+    $("#page21").click(function() {
+        $('#magazine').turn("page", 21)
+        removeNextPrev()
+    });
+
+    $("#page22").click(function() {
+        $('#magazine').turn("page", 22)
+        removeNextPrev()
+    });
+
+    $("#page23").click(function() {
+        $('#magazine').turn("page", 23)
+        removeNextPrev()
+    });
+
+    $("#page24").click(function() {
+        $('#magazine').turn("page", 24)
         removeNextPrev()
     });
 
     $("#start-clear").on("click", function () {
-        $('#magazine').turn("page", 3)
+        $('#magazine').turn("page", 2)
         removeNextPrev()
     })
 
     $("#clear-tab").on("click", function () {
-        $('#magazine').turn("page", 3)
+        $('#magazine').turn("page", 2)
         removeNextPrev()
     })
     
     $("#connected-tab").on("click", function () {
-        $('#magazine').turn("page", 9)
+        $('#magazine').turn("page", 25)
         removeNextPrev()
     })
 
     $("#capable-tab").on("click", function () {
-        $('#magazine').turn("page", 10)
+        $('#magazine').turn("page", 26)
         removeNextPrev()
     })
 
     $("#courageous-tab").on("click", function () {
-        $('#magazine').turn("page", 11)
+        $('#magazine').turn("page", 27)
         removeNextPrev()
     })
 
@@ -100,13 +154,13 @@ $(document).ready(function() {
 
 function removeNextPrev() {
     const page = $('#magazine').turn('view')
-    if (page[0] === 0 || page[0] === 9 || page[0] === 10 || page[0] === 11) {
+    if (page[0] === 0 || page[0] === 25 || page[0] === 26 || page[0] === 27) {
         $("#next").css({"display":"none"})
         $("#previous").css({"display":"none"})
         $("#magazine").css({"z-index":20});
         $("#navigate-cover").css({"z-index": 30})
         $("#navigate-to-home").css({"z-index":30})
-    } else if (page[0] === 8) {
+    } else if (page[0] === 24) {
         $("#next").css({"display":"none"})
         $("#previous").css({"display":"block"})
         $("#navigate-to-home").css({"z-index":0})
