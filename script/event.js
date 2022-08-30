8c53fa026f948bfb4fc51d047c8c7aaae8bbe349
azputra <ahmadzp102@gmail.com>
2022-08-30 14:42:45 +0700

    update page & update event

--- script/event.js
+++ script/event.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // $("#magazine").css({"margin-top":screen.height/2});
 
     $("#magazine").bind('start', function (event, pageObject, corner){
-        if (pageObject.page === 24 || pageObject.page === 25 || pageObject.page === 26 || pageObject.page === 27) {
+        if (pageObject.page === 18 || pageObject.page === 19 || pageObject.page === 20 || pageObject.page === 21) {
             if (corner == 'tl' || corner == 'tr' || corner == 'bl' || corner == 'br'){
                 event.preventDefault();
             }
@@ -20,25 +20,85 @@ $(document).ready(function() {
             turned: function(e, page) {
                     var audio = new Audio('assets/sound1.mp3');
                     audio.play();
+                    console.log(page);
                     if (page === 1) {
                         $("#magazine").css({"z-index":20});
                         $("#navigate-cover").css({"z-index": 30})
                         $("#index-clear").css({"z-index": 0})
-                    } else if (page === 4) {
+                        clearLinkReference()
+                    } else if (page === 5) {
                         $("#index-clear").css({"z-index": 30})
+                       clearLinkReference()
+                    } else if (page === 8) {
+                        clearLinkReference()
+                        $("#index-clear").css({"z-index": 0})
+                        $("#magazine").css({"z-index":0});
+                        $("#navigate-cover").css({"z-index": 0})
+                        $("#link-page-8").css({"z-index":30})
+                        $("#link-page-8-container").css({"z-index":30})
+                    } else if (page === 9) {
+                        clearLinkReference()
+                        $("#index-clear").css({"z-index": 0})
+                        $("#magazine").css({"z-index":0});
+                        $("#navigate-cover").css({"z-index": 0})
+                        $("#link-page-9").css({"z-index":30})
+                        $("#link-page-9-container").css({"z-index":30})
+                    } else if (page === 10) {
+                        clearLinkReference()
+                        $("#index-clear").css({"z-index": 0})
+                        $("#magazine").css({"z-index":0});
+                        $("#navigate-cover").css({"z-index": 0})
+                        $("#link-page-10").css({"z-index":30})
+                        $("#link-page-10-container").css({"z-index":30})
+                    } else if (page === 11) {
+                        clearLinkReference()
+                        $("#index-clear").css({"z-index": 0})
+                        $("#magazine").css({"z-index":0});
+                        $("#navigate-cover").css({"z-index": 0})
+                        $("#link-page-11").css({"z-index":30})
+                        $("#link-page-11-container").css({"z-index":30})
+                    } else if (page === 15) {
+                        clearLinkReference()
+                        $("#index-clear").css({"z-index": 0})
+                        $("#magazine").css({"z-index":0});
+                        $("#navigate-cover").css({"z-index": 0})
+                        $("#link-page-15").css({"z-index":30})
+                        $("#link-page-15-container").css({"z-index":30})
+                    } else if (page === 18) {
+                        clearLinkReference()
+                        $("#index-clear").css({"z-index": 0})
+                        $("#magazine").css({"z-index":0});
+                        $("#navigate-cover").css({"z-index": 0})
+                        $("#link-page-18").css({"z-index":30})
+                        $("#link-page-18-container").css({"z-index":30})
                     } else {
                         $("#index-clear").css({"z-index": 0})
                         $("#magazine").css({"z-index":0});
                         $("#navigate-cover").css({"z-index": 0})
+                        clearLinkReference()
                     }
+
                 // console.log('Current view: ', $(this).turn('view'));
             }
         }
     });
 
-    $("#home").click(function() {
+    $("#home-icon").click(function() {
         $('#magazine').turn("page", 1)
     });
+
+    $("#clear-icon").on("click", function () {
+        $('#magazine').turn("page", 4)
+        removeNextPrev()
+    })
+
+    $("#yammer-icon").on("click", function () {
+        window.open('https://web.yammer.com/main/org/optus.com.au/groups/eyJfdHlwZSI6Ikdyb3VwIiwiaWQiOiIxODc3MzMxOTY4MCJ9', '_blank');
+    })
+
+    $("#email-icon").on("click", function () {
+        window.location.href = "mailto:Steve.Reynolds@optus.com.au";
+    })
 
     $("#next").on("click", function (e) {
         $('#magazine').turn('next')
@@ -60,13 +120,18 @@ $(document).ready(function() {
         }
     })
 
-    $("#page5").click(function() {
-        $('#magazine').turn("page", 5)
+    $("#page6").click(function() {
+        $('#magazine').turn("page", 6)
         removeNextPrev()
     });
 
-    $("#page6").click(function() {
-        $('#magazine').turn("page", 6)
+    $("#page7").click(function() {
+        $('#magazine').turn("page", 7)
+        removeNextPrev()
+    });
+
+    $("#page8").click(function() {
+        $('#magazine').turn("page", 8)
         removeNextPrev()
     });
 
@@ -75,8 +140,18 @@ $(document).ready(function() {
         removeNextPrev()
     });
 
-    $("#page11").click(function() {
-        $('#magazine').turn("page", 11)
+    $("#page10").click(function() {
+        $('#magazine').turn("page", 10)
+        removeNextPrev()
+    });
+
+    $("#page12").click(function() {
+        $('#magazine').turn("page", 12)
+        removeNextPrev()
+    });
+
+    $("#page13").click(function() {
+        $('#magazine').turn("page", 13)
         removeNextPrev()
     });
 
@@ -85,38 +160,18 @@ $(document).ready(function() {
         removeNextPrev()
     });
 
+    $("#page15").click(function() {
+        $('#magazine').turn("page", 15)
+        removeNextPrev()
+    });
+
     $("#page16").click(function() {
         $('#magazine').turn("page", 16)
         removeNextPrev()
     });
 
-    $("#page17").click(function() {
-        $('#magazine').turn("page", 17)
-        removeNextPrev()
-    });
-
-    $("#page19").click(function() {
-        $('#magazine').turn("page", 19)
-        removeNextPrev()
-    });
-
-    $("#page21").click(function() {
-        $('#magazine').turn("page", 21)
-        removeNextPrev()
-    });
-
-    $("#page22").click(function() {
-        $('#magazine').turn("page", 22)
-        removeNextPrev()
-    });
-
-    $("#page23").click(function() {
-        $('#magazine').turn("page", 23)
-        removeNextPrev()
-    });
-
-    $("#page24").click(function() {
-        $('#magazine').turn("page", 24)
+    $("#page18").click(function() {
+        $('#magazine').turn("page", 18)
         removeNextPrev()
     });
 
@@ -129,19 +184,19 @@ $(document).ready(function() {
         $('#magazine').turn("page", 2)
         removeNextPrev()
     })
-    
+
     $("#connected-tab").on("click", function () {
-        $('#magazine').turn("page", 25)
+        $('#magazine').turn("page", 19)
         removeNextPrev()
     })
 
     $("#capable-tab").on("click", function () {
-        $('#magazine').turn("page", 26)
+        $('#magazine').turn("page", 20)
         removeNextPrev()
     })
 
     $("#courageous-tab").on("click", function () {
-        $('#magazine').turn("page", 27)
+        $('#magazine').turn("page", 21)
         removeNextPrev()
     })
 
@@ -152,15 +207,30 @@ $(document).ready(function() {
     })
 });
 
+function clearLinkReference() {
+    $("#link-page-8").css({"z-index":0})
+    $("#link-page-8-container").css({"z-index":0})
+    $("#link-page-9").css({"z-index":0})
+    $("#link-page-9-container").css({"z-index":0})
+    $("#link-page-10").css({"z-index":0})
+    $("#link-page-10-container").css({"z-index":0})
+    $("#link-page-11").css({"z-index":0})
+    $("#link-page-11-container").css({"z-index":0})
+    $("#link-page-15").css({"z-index":0})
+    $("#link-page-15-container").css({"z-index":0})
+    $("#link-page-18").css({"z-index":0})
+    $("#link-page-18-container").css({"z-index":0})
+}
+
 function removeNextPrev() {
     const page = $('#magazine').turn('view')
-    if (page[0] === 0 || page[0] === 25 || page[0] === 26 || page[0] === 27) {
+    if (page[0] === 0 || page[0] === 19 || page[0] === 20 || page[0] === 21) {
         $("#next").css({"display":"none"})
         $("#previous").css({"display":"none"})
         $("#magazine").css({"z-index":20});
         $("#navigate-cover").css({"z-index": 30})
         $("#navigate-to-home").css({"z-index":30})
-    } else if (page[0] === 24) {
+    } else if (page[0] === 18) {
         $("#next").css({"display":"none"})
         $("#previous").css({"display":"block"})
         $("#navigate-to-home").css({"z-index":0})
